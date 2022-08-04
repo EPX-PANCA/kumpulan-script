60c2221b93d2d1d1a87b1eef1e3c3ce1bd26499b
PANCA PUTRA PAHLAWAN <rectapanca@gmail.com>
2022-08-04 21:31:38 +0700

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-<script>
-  if(localStorage.getItem("theme")==undefined){
-    localStorage.setItem("theme","dark");
-    (document.querySelector("html").className="dark-mode");
-  }else{
-    console.log("false");
-  }
-</script>
