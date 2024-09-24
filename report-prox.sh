a2042ee6b994b3c613543f8870a254c7be56b939
PANCA PUTRA PAHLAWAN <rectapanca@gmail.com>
2024-09-24 15:21:20 +0700

    Create report-prox.sh

---
+++ report-prox.sh
@@ -0,0 +1,25 @@
+for vmid in $(qm list | awk 'NR>1 {print $1}'); do
+  echo "========================================"
+  echo "VM ID      : $vmid"
+  echo "----------------------------------------"
+  # Nama VM
+  vm_name=$(qm list | grep $vmid | awk '{print $2}')
+  echo "Nama VM    : $vm_name"
+  # Status VM
+  vm_status=$(qm status $vmid | grep status | awk '{print $2}')
+  echo "Status     : $vm_status"
+  # CPU dan Memori
+  cpu_info=$(qm config $vmid | grep -E '(cpu|cores)' | awk -F ': ' '{print $2}')
+  mem_info=$(qm config $vmid | grep memory | awk -F ': ' '{print $2}')
+  echo "CPU        : $cpu_info"
+  echo "Memory     : $mem_info MB"
+  # Storage
+  storage_info=$(qm config $vmid | grep disk)
+  if [ -z "$storage_info" ]; then
+    echo "Storage    : Tidak ada disk"
+  else
+    echo "Storage    : $storage_info"
+  fi
+  echo "========================================"
+  echo ""
+done
